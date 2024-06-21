01898f13efd49972b9ecc6d86f0bafb65e90a62c
julsco <juliancscotti@gmail.com>
2024-06-21 20:52:04 +0100

    changes in wording

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,39 +5,62 @@ import { HiArrowLongDown } from "react-icons/hi2";
 import { WindowWidthContext } from "../../App";
 import { Link } from "react-router-dom";
 
-
 function About(props, ref) {
+  const windowWidth = useContext(WindowWidthContext);
 
-    const windowWidth = useContext(WindowWidthContext);
+  return (
+    <div
+      ref={ref}
+      className="about__section overflow-hidden flex flex-col items-center justify-center w-full xl:px-32 bg-white pt-10"
+    >
+      <div className="h-7/8 flex flex-col md:flex-row items-center justify-center gap-6 md:px-10 lg:px-20">
+        <img
+          src={doctor}
+          alt="fernando"
+          className="w-52 md:w-auto md:h-96 rounded-xl shadow-slate-800 shadow-2xl"
+        />
 
-    return (
-
-        <div ref={ref} className="about__section overflow-hidden flex flex-col items-center justify-center w-full xl:px-32 bg-white pt-10">
-                
-                    <div className="h-7/8 flex flex-col md:flex-row items-center justify-center gap-6 md:px-10 lg:px-20">
-                        
-                        <img src={doctor} alt="fernando" className="w-52 md:w-auto md:h-96 rounded-xl shadow-slate-800 shadow-2xl" />
-                        
-                        <div className="flex flex-col gap-4 ">
-                            <h1 className="text__green font-bold text-2xl xl:text-3xl self-start pl-6" >The equilibria Project</h1>
-                            <div className="text__green text-base xl:text-xl px-6">
-                                Fernando Martin Riggi Bou is a Sports Therapist working in the Sport Industry as an Instructor, Chiropractor, Coach, Nutritionist, Personal Trainer, and Rehabilitation Therapist. After working in many sports clubs such as Spartans FC he decided to continue his professional career by working for his-self in parallel with his clients.
-                            </div>
-                            <div className="self-center md:self-start md:pl-6 md:pt-3">
-                                <Link to="/about"><button className="btn__main bg__red text-center text-white rounded w-32 h-10 text-base" >Explore more</button></Link>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="flex flex-col items-center h-1/8 pt-6 md:pt-20 xl:pt-36 lg:pt-12">
-                        <HiArrowLongDown onClick={props.scrollDown} className="text__green text-6xl cursor-pointer" />
-                        <div onClick={props.scrollDown} className="text__green h-16 font-bold cursor-pointer">Scroll down</div>
-                    </div>
-                
-        </div>  
-
-            
-            
-    )
+        <div className="flex flex-col gap-4 ">
+          <h1 className="text__green font-bold text-2xl xl:text-3xl self-start pl-6">
+            The equilibria Project
+          </h1>
+          <div className="text__green text-base xl:text-xl px-6">
+            Welcome to Fernando Martin Riggi Bou's Sports Medicine Practice,
+            where your health and athletic performance are my top priorities.
+            With years of experience in sports medicine, I specialize in injury
+            prevention, treatment, and rehabilitation for athletes of all
+            levels. Combining advanced techniques with personalized care, I am
+            dedicated to helping you achieve your peak performance and recover
+            swiftly from any setbacks. Whether you're a professional athlete or
+            a fitness enthusiast, you can trust my expertise to support your
+            journey to optimal health and performance. After working in many
+            sports clubs such as Spartans FC, I decided to continue my
+            professional career by working for myself in parallel with my
+            clients.
+          </div>
+          <div className="self-center md:self-start md:pl-6 md:pt-3">
+            <Link to="/about">
+              <button className="btn__main bg__red text-center text-white rounded w-32 h-10 text-base">
+                Explore more
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col items-center h-1/8 pt-6 md:pt-20 xl:pt-36 lg:pt-12">
+        <HiArrowLongDown
+          onClick={props.scrollDown}
+          className="text__green text-6xl cursor-pointer"
+        />
+        <div
+          onClick={props.scrollDown}
+          className="text__green h-16 font-bold cursor-pointer"
+        >
+          Scroll down
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default forwardRef(About)
+export default forwardRef(About);
--- src/components/About/MainAbout.jsx
+++ src/components/About/MainAbout.jsx
@@ -3,89 +3,104 @@ import Nav from "../Nav/Nav";
 import Footer from "../Footer/Footer";
 import doctor from "../../assets/doctor.jpg";
 import { BiRun } from "react-icons/bi";
-import { GiMuscleUp, GiMeditation, GiHealing, GiSkeleton } from "react-icons/gi";
+import {
+  GiMuscleUp,
+  GiMeditation,
+  GiHealing,
+  GiSkeleton,
+} from "react-icons/gi";
 import { TbMassage, TbPhysotherapist } from "react-icons/tb";
 import { FaNutritionix } from "react-icons/fa";
 
-export default function MainAbout () {
+export default function MainAbout() {
+  const solutions = [
+    {
+      title: "Sports Medicine",
+      icon: TbPhysotherapist,
+    },
+    {
+      title: "Osteopathic",
+      icon: GiSkeleton,
+    },
+    {
+      title: "Fitness",
+      icon: BiRun,
+    },
 
-    const solutions = [
-        {
-            title: "Chiropractic",
-            icon: GiSkeleton
-        },
-        {
-            title: "Fitness",
-            icon: BiRun
-        },
-        
-        {
-            title: "Meditation",
-            icon: GiMeditation
-        },
-       
-       
-        {
-            title: "Physotherapist",
-            icon: TbPhysotherapist
-        },
-        {
-            title: "Nutrition",
-            icon: FaNutritionix
-        },
-        {
-            title: "Sport\n Massage",
-            icon: TbMassage
-        },
-        {
-            title: "Post-injured\n Recovery",
-            icon: GiHealing
-        },
-        {
-            title: "Personal\n Trainer",
-            icon: GiMuscleUp
-        },
-    ]
+    {
+      title: "Meditation",
+      icon: GiMeditation,
+    },
+    {
+      title: "Nutrition",
+      icon: FaNutritionix,
+    },
+    {
+      title: "Sport\n Massage",
+      icon: TbMassage,
+    },
+    {
+      title: "Post-injury\n Recovery",
+      icon: GiHealing,
+    },
+    {
+      title: "Personal\n Trainer",
+      icon: GiMuscleUp,
+    },
+  ];
 
-    return (
-        <>
-            <Nav showBg={false}/>
-            
-            <h1 className="text__green text-3xl lg:text-4xl font-bold pt-12 text-center">About me</h1>
-            <div className="flex flex-col md:flex-row gap-7 justify-center items-center py-12 px-2 md:px-8 lg:px-16 xl:px-32">
+  return (
+    <>
+      <Nav showBg={false} />
 
+      <h1 className="text__green text-3xl lg:text-4xl font-bold pt-12 text-center">
+        About me
+      </h1>
+      <div className="flex flex-col md:flex-row gap-7 justify-center items-center py-12 px-2 md:px-8 lg:px-16 xl:px-32">
+        <img
+          src={doctor}
+          alt="fernando"
+          className="w-52 md:w-auto md:h-96 rounded-xl shadow-slate-800 shadow-2xl"
+        />
 
-                <img src={doctor} alt="fernando" className="w-52 md:w-auto md:h-96 rounded-xl shadow-slate-800 shadow-2xl" />
+        <p className="px-6 leading-7 lg:leading-8 text-base lg:text-xl">
+          Welcome to Fernando Martin Riggi Bou's Sports Medicine Practice, where
+          your health and athletic performance are my top priorities. With years
+          of experience in sports medicine, I specialize in injury prevention,
+          treatment, and rehabilitation for athletes of all levels. Combining
+          advanced techniques with personalized care, I am dedicated to helping
+          you achieve your peak performance and recover swiftly from any
+          setbacks. Whether you're a professional athlete or a fitness
+          enthusiast, you can trust my expertise to support your journey to
+          optimal health and performance. After working in many sports clubs
+          such as Spartans FC, I decided to continue my professional career by
+          working for myself in parallel with my clients.
+        </p>
+      </div>
 
-                <p className="px-6 leading-7 lg:leading-8 text-base lg:text-xl">
-                    Fernando Martin Riggi Bou is a Sports Therapist working in the Sport Industry as an Instructor, Coach, Nutritionist, Personal Trainer, and Rehabilitation Therapist. After working in many sports clubs such as Spartans FC with kids and adults, outstanding rugbiers, cricketers, hockey teams, boxing, and Thai boxing fighters, he decided to continue his professional career by working for his-self in parallel with his clients. The different kind of physical injuries presented by the athletes coming from the most varied sports has taken him in the way to continue his education in different areas to understand the necessary rehabilitation techniques in order to treat the patients. Among the treatments deliver, we can find muscular-skeletal, nervous, digestive, and becoming an expert in spinal treatments.
-                </p>
+      <div className="flex flex-col gap-12 justify-center items-center py-12">
+        <h1 className="text__green text-3xl lg:text-4xl font-bold">
+          Solutions
+        </h1>
 
-                </div>
+        <div className="flex flex-row flex-wrap justify-around items-center gap-10 px-6 lg:px-32 xl:w-4/6">
+          {solutions.map((solution, i) => (
+            <div
+              key={i}
+              className="flex flex-col gap-3 items-center justify-center w-32 lg:w-40 h-32 lg:h-40"
+            >
+              <div className="bg__red flex justify-center items-center rounded-full w-16 lg:w-24 h-16 lg:h-24 hover:opacity-80">
+                <solution.icon className="text-white text-3xl lg:text-4xl " />
+              </div>
 
-                <div className="flex flex-col gap-12 justify-center items-center py-12">
-                <h1 className="text__green text-3xl lg:text-4xl font-bold">Solutions</h1>
-
-                <div className="flex flex-row flex-wrap justify-around items-center gap-10 px-6 lg:px-32 xl:w-4/6">
-
-                    {solutions.map((solution, i) => (
-                        <div key={i} className="flex flex-col gap-3 items-center justify-center w-32 lg:w-40 h-32 lg:h-40">
-                        
-                            <div className="bg__red flex justify-center items-center rounded-full w-16 lg:w-24 h-16 lg:h-24 hover:opacity-80">
-                                <solution.icon className="text-white text-3xl lg:text-4xl "/>
-                            </div>
-
-                            <pre><div className="text-base text-center">{solution.title}
-                            </div></pre>
-
-                        </div>
-                            ))}
-                    
-                </div>
+              <pre>
+                <div className="text-base text-center">{solution.title}</div>
+              </pre>
             </div>
-            <Footer />
-            
-
-        </>
-    )
+          ))}
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
